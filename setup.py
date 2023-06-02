cc134b5363a2fd75a1017906bcb9573a9dc0bc3f
Keaton Townley <112629266+wktown@users.noreply.github.com>
2023-06-01 22:10:03 -0400

    Update setup.py
    URL to this repo

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     long_description=readme,
     author="Brain-Score Team",
     author_email='mschrimpf@mit.edu, jjpr@mit.edu',
-    url='https://github.com/brain-score/brain-score',
+    url='https://github.com/wktown/brain-score.git@ridge',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=requirements,
